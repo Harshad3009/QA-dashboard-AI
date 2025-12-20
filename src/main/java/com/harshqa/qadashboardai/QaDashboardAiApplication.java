585e1860de8f3c25e8ebbd56ab4abf66aebfb4b2
HarshadforLoco <harshad.bodekar@loco.gg>
2025-12-20 13:23:29 +0530

    initial commit
    Initialized the spring project for the qa dashboard ai backend logic

---
+++ src/main/java/com/harshqa/qadashboardai/QaDashboardAiApplication.java
@@ -0,0 +1,13 @@
+package com.harshqa.qadashboardai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaDashboardAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QaDashboardAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/harshqa/qadashboardai/QaDashboardAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshqa.qadashboardai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaDashboardAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
