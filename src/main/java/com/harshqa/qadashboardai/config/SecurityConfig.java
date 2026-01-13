2d0391a5f1318eb9ca88ddb536deb50554f921fc
Harshad3009 <harshoo3009@gmail.com>
2026-01-13 18:45:18 +0530

    Updated cors configs

--- src/main/java/com/harshqa/qadashboardai/config/SecurityConfig.java
+++ src/main/java/com/harshqa/qadashboardai/config/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000")); // Frontend URLs
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000", "http://localhost:8080")); // Frontend URLs
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
